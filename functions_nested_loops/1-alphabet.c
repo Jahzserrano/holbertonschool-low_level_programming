a47f2d58429bc565469aa674d6626f37caeb09d4
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-16 15:37:48 -0400

    deleted main and putchar

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,24 +1,9 @@
-#include <unistd.h>
-#include <stdio.h>
 #include "main.h"
 /**
- * main - check the code
+ * print alphabet - print the alphabet
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/* print any char */
-int _putchar(char c)
-{
-        return (write(1, &c, 1));
-}
-
-/* print the alphabet */
 void print_alphabet(void)
 {
         char ch;
@@ -27,5 +12,5 @@ void print_alphabet(void)
         {
                _putchar(ch);
         }
-       _putchar('\n');
+	_putchar('\n');
 }
