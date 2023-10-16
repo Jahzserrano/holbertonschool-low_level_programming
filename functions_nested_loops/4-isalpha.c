879e9bf31ab8eeffa2eac8e24ad756108cfaa77a
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-16 16:29:58 -0400

    added task #4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - Checks if c is a alpha
+ * @c: character to be checked.
+ * Return: 0 or 1.
+ */
+int _isalpha(int c)
+{
+	/* A-Z 65-90 | a-z 97-122 */
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
