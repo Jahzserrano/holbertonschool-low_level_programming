389e8553fac674a478bd6aa4a7fc3925c926f3c3
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-16 12:45:37 -0400

    added prototype to main.h and edited prototype inside 1-alphabet

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,14 +2,5 @@
 #define _main_h
 #include <stdio.h>
 
-void print_alphabet(void)
-{
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-}
+void print_alphabet(void);
 #endif
