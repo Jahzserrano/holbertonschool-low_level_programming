4e26d17685d06302c484549a261187d067587a49
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-16 12:49:15 -0400

    relocate print_alphabet function

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,19 +5,18 @@
  *
  * Return: Always 0.
  */
+void print_alphabet(void)
+{
+        char ch;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+                putchar(ch);
+        }
+        putchar('\n');
+}
 int main(void)
 {
 	print_alphabet();
 	return (0);
 }
-void print_alphabet(void)
-{
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-}
-
