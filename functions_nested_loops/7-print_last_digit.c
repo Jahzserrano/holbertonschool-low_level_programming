a4242ba84418394b3f09eb2b0e3997ee82428ec0
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-17 09:49:04 -0400

    print for each if statement

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -12,15 +12,17 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		ld = ((-1 * n) % 10);
+		_putchar(ld + '0');
 	}
 	else if (n == INT_MIN)
 	{
 		ld = -(n % 10);
+		_putchar(ld - '0');
 	}
 	else
 	{
 		ld = (n % 10);
+		_putchar(ld + '0');
 	}
-	_putchar(ld + '0');
 	return (ld);
 }
