d748b69be864fb02fe71d85c7b3e9e5e404114b3
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-17 15:04:01 -0400

    added newlines

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,21 +11,23 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
-			printf("%d", n);
+			printf("%d, ", i);
 		}
+		printf("98\n");
 	}
 	else if (n == 98)
 	{
-		printf("%d", n);
+		printf("%d\n", n);
 	}
 	else if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d", n);
+			printf("%d, ", i);
 		}
+		printf("98\n");
 	}
 	printf("\n");
 }
