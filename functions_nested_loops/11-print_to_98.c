24867b4543bbefd48094afa601fe246a2f610253
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-17 14:53:56 -0400

    added space

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,14 @@
 #include "main.h"
-
+#include <stdio.h>
+/**
+ * print_to_98 - function that prints all natural nuumbers from n to 98
+ * @n: number to checked
+ * Return: void
+ */
 void print_to_98(int n)
 {
 	int i;
+
 	if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
