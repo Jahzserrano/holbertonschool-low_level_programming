eb233ea2063be41dfb7d0eccd5e9d4045db6ac2a
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-17 13:57:55 -0400

    added task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,23 +14,23 @@ void times_table(void)
 			k = j * i;
 			if (j == 0)
 			{
-				putchar(k + '0');
+				_putchar(k + '0');
 			}
 			if (k < 10 && j != 0)
 			{
-				putchar(',');
-				putchar(' ');
-				putchar(' ');
-				putchar(k + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
 			}
 			else if (k >= 10)
 			{
-				putchar(',');
-				putchar(' ');
-				putchar((k / 10) + '0');
-				putchar((k % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
