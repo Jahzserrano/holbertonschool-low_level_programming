d5f9f366713c828a45083c12997800fc63ca4c21
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-18 14:05:01 -0400

    added task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_triangle - function that prints a triangle.
+ * @size: is the size of the trangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int w;
+	int h;
+	int c = 1;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (h = 0; h < size; h++)
+	{
+		for (w = 0; w < size; w++)
+		{
+			if ((size - h) > 0 && (size - w) > 0)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_puthchar('#');
+			}
+
+		}
+	}
+
+}
