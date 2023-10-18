959b085a0dc7f71d7c2ea269e2fb04f89947eeed
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-18 13:18:31 -0400

    added task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int i =  0; 
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz\n")
+		}
+		if ((i % 3) == 0 && (i % 5) != 0)
+		{
+			printf("Fizz\n");
+		}
+		else if ((i % 5) == 0 && (i % 3) != 0)
+		{
+			printf("Buzz\n")
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
+	}
+}
