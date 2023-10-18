cff2d31588b81cab3e354b9d861fa5843e2020c5
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-18 13:27:07 -0400

    added task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	int i =  0;
+	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		if ((i % 3) == 0 && (i % 5) != 0)
+		else if ((i % 3) == 0 && (i % 5) != 0)
 		{
 			printf("Fizz ");
 		}
