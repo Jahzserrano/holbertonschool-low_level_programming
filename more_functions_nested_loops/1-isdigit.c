3e13cba3554e759dc94cbca7c4f9eacea69c146c
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-18 11:15:18 -0400

    added task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - functions that checks for a digit ( 0 through 9)
+ * @c: digit t obe checked.
+ *
+ * Return: 1 if it's a number otherwise return 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 58)
+	{
+		return (1); /* Successful */
+	}
+	else
+	{
+		return (0);
+	}
+
+}
