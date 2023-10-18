65975c8c6bd7ef96afb7557790cc59c32e428463
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-18 11:20:42 -0400

    added task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - functions that multplies two integers.
+ * @a: first integer.
+ * @b: second integer.
+ * Return: The product of two integers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
