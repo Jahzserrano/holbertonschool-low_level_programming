7acd223ac34225e33a987b78d9279fccd20d5dca
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-18 12:39:05 -0400

    added task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - function that prints a square.
+ * @size = is the size of the square.
+ *
+ * Return: void.
+ */
+void print_square(int size)
+{
+	int width;
+	int height;
+	
+	for (height = 0; height <= size; height++)
+	{
+		for (width = 0; width <= size; width++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
