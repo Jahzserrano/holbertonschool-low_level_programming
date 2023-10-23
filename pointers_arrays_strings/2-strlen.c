ece52c636b6f7d6569953144ed8db0e022d256b5
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-23 09:44:23 -0400

    added task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: holds memory address
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		p++;
+	}
+	return (count);
+}
