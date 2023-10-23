84157ccd583480d62d8e17cdf085ba8c8622b293
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-23 14:24:42 -0400

    edited 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,9 @@
 #include "main.h"
+
 /**
- * puts2 - function that prints every other character of a string.
- * @str: holds the string to be printed.
- *
- * Return: void
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
  */
 void puts2(char *str)
 {
