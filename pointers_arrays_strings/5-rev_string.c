50cd38bdd215145ff56e3b2a90abc3b90840b0aa
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-23 14:33:02 -0400

    Edited task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,23 @@
  */
 void rev_string(char *s)
 {
-	int len, i;
-	char *end, *begin, tmp;
+	char tmp;
+	int i, len, len1;
 
-	len = strlen(s);
-	
-	begin = s;
-	end = s + len - 1;
+	len = 0;
+	len1 = 0;
 
-	for (i = 0; i < (len - 1) / 2; i++)
+	while (s[len] != '\0')
 	{
-		tmp = *begin;
-		*begin = *end;
-		*end = tmp;
-
-		begin++;
-		end--;
+		len++;
 	}
 
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len--] = tmp;
+	}
 }
