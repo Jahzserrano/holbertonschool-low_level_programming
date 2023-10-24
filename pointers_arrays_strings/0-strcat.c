52fc26279b9815b7c45a84d8f8ed2c595a2fbf3d
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-24 09:21:54 -0400

    added task 0 proyect 2

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat -  function that concatenates two strings.
+ * @dest: string to concatenated
+ * @src: string to concatenated
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len_d, len_s;
+
+	len_d = strlen(dest);
+	len_s = 0;
+
+	while (src[len_s] != '\0')
+	{
+		dest[len_d + len_s] = src[len_s];
+		len_s++;
+	}
+	return (dest);
+}
