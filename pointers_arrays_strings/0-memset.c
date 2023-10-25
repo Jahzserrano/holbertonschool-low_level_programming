03fc64e3fac87a4545b8716440d66a20434b2804
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-25 11:05:46 -0400

    added task 0 proyect 3

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _memset - a function that fills memory with a constant byte.
- * @s:
- * @b:
- * @n:
+ * @s: string to be edited.
+ * @b: byte to be enter in memory.
+ * @n: the number of memery it will take.
  * Return: a pointer to the memory area s.
  */
 char *_memset(char *s, char b, unsigned int n)
