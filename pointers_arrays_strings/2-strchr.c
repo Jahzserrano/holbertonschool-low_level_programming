13155b4f62510996f9df2fc619193d03c7384bd5
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-25 12:08:11 -0400

    edited code task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,7 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s)
-	{
-		if (*s == c)
-			return (s);
-		if (!*s)
-			return (NULL);
-		s++;
-	}
-	return (NULL);
+	while (*s && *s != c)
+		++s;
+	return ((char *)(c == *s ? s : NULL));
 }
