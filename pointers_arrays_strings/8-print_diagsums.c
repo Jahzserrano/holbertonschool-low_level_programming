39f6a883125db8dfcf317feffc0a36f02a209dcb
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-26 10:44:16 -0400

    added task 7 proyect 3

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -14,7 +14,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum += *((a + i * size) +  i);
-		sum1 += *((a + i * size) + (size - i - 1);
+		sum1 += *((a + i * size) + (size - i - 1));
 	}
 	printf("%d, %d\n", sum, sum1);
 }
