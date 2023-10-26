42752f711fffa886a5ea60708e8c7a7b6b484dd6
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-26 10:43:16 -0400

    added task 7 proyect 3

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - a function that prints the sum of the two diagnals
+ * @a: string
+ * @size: integer
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum = 0;
+	int sum1 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum += *((a + i * size) +  i);
+		sum1 += *((a + i * size) + (size - i - 1);
+	}
+	printf("%d, %d\n", sum, sum1);
+}
