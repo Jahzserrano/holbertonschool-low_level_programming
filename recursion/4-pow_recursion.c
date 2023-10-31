42bcdb3397178694e7694db89b5d2fadca62dce0
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-10-31 10:26:39 -0400

    added task 4 recursion proyect

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - a function that calculated the power x raised by y
+ * @x: value to be raised
+ * @y: the power to be raised
+ * Return: the product of the x raised by the power of y.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return 1;
+	else
+		return x * _pow_recursion(x, y - 1);
+}
