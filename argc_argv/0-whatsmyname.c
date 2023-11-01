2605ddaca70918c61aff5f91b87b9d99f135560b
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-11-01 11:16:42 -0400

    fix description error from betty task 0 argc and argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * whatsmyname - a function that prints its name
+ * main - Entry Point
  * @argc: the count of arguments
- * @argv: array of strings
+ * @argv: array of strings.
+ * Return: Always 0.
  */
 
 int main(int argc, char **argv)
 {
 	whatsmyname(argv, argc - 1);
 	return (0);
-
-}	
-
+}
+/**
+ * whatsmyname - a function that prints the executable's name.
+ * @s: array of string.
+ * @c: index where name is.
+ */
 void whatsmyname(char **s, int c)
 {
 	printf("%s\n", s[c]);
